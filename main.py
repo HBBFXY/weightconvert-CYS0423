ad571f6a98e01ea160370639b59fd0477145c584
CYS0423 <3515415214@qq.com>
2025-09-10 17:43:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 # 在这个文件下编写代码，题目具体要求见README.md文件
+weight_input = input()
+num = float(weight_input[:-2])
+unit = weight_input[-2:]
+if unit == "kg":
+    result = num * 2.2046
+    print(f"对应的英制重量为{result:.3f}磅")
+elif unit == "pd":
+    result = num / 2.2046
+    print(f"对应的公制重量为{result:.3f}公斤")
