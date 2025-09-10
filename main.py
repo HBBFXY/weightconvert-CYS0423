aae872a095a942940d012bc8ab91e3b37542f901
CYS0423 <3515415214@qq.com>
2025-09-10 18:00:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ num = float(weight_input[:-2])
 unit = weight_input[-2:].lower()
 if unit == "kg":
     result = num * 2.2046
-    print(f"对应的英制重量为{result:.3f}磅")
+    print("对应的英制重量为{:.3f}磅".format(result))
 elif unit == "pd":
     result = num / 2.2046
-    print(f"对应的公制重量为{result:.3f}公斤")
+    print("对应的公制重量为{:.3f}公斤".format(result))
