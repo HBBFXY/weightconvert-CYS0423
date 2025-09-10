6e28f11d861e2c611e6879c7dd112075a1f8123f
CYS0423 <3515415214@qq.com>
2025-09-10 17:58:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 weight_input = input()
 num = float(weight_input[:-2])
-unit = weight_input[-2:].lower() 
+unit = weight_input[-2:].lower()
 if unit == "kg":
     result = num * 2.2046
     print(f"对应的英制重量为{result:.3f}磅")
